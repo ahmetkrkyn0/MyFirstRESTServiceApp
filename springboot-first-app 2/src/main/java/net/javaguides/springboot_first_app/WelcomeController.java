9b4c71a1a9cabc7f897b1031cddd78c5449ea690
ahmetkrkyn0 <ahmet.karakoyun06@gmail.com>
2025-07-04 10:12:15 +0300

    bug fixed

--- springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/WelcomeController.java
+++ springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/WelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/customers") // Tüm endpointler için bir ana yol tanımladım
 public class WelcomeController {
 
     @Autowired
