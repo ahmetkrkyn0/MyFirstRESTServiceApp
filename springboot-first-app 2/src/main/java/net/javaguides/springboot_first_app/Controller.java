abb5b8ddda628e3360fc9168e6280b9545ca9acf
ahmetkrkyn0 <ahmet.karakoyun06@gmail.com>
2025-07-08 11:08:18 +0300

    Update Controller.java

--- springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/Controller.java
+++ springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/Controller.java
@@ -1,43 +1,57 @@
 package net.javaguides.springboot_first_app;
 
+import net.javaguides.springboot_first_app.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/customers") // Tüm endpointler için bir ana yol tanımladım
+@RequestMapping("/api/customers")
 public class Controller {
 
+    private final CustomerService customerService;
+
     @Autowired
-    private CustomerRepository customerRepository;
+    public Controller(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @PostMapping("/add")
     public String addCustomer(@RequestParam String first, @RequestParam String last) {
         Customer customer = new Customer();
         customer.setFirstName(first);
         customer.setLastName(last);
-        customerRepository.save(customer);
+        customerService.saveCustomer(customer);
         return "Added new customer to repo!";
     }
 
     @GetMapping("/list")
     public Iterable<Customer> getCustomers() {
-        return customerRepository.findAll();
+        return customerService.getAllCustomers();
     }
 
     @GetMapping("/find/{id}")
-    public Customer findCustomerById(@PathVariable Integer id) {
-        return customerRepository.findCustomerById(id);
+    public ResponseEntity<Customer> findCustomerById(@PathVariable Integer id) {
+        Customer customer = customerService.getCustomerById(id);
+        return ResponseEntity.ok(customer);
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteCustomerByID(@PathVariable Integer id) {
-        customerRepository.deleteById(id);
-        return "Deleted customer with id: " + id;
+    public ResponseEntity<String> deleteCustomerByID(@PathVariable Integer id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok("Deleted customer with id: " + id);
     }
 
     @PostMapping
-    public String addCustomer(@RequestBody Customer customer) {
-        customerRepository.save(customer);
-        return "Added new customer to repo!";
+    public ResponseEntity<String> addCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return new ResponseEntity<>("Added new customer to repo!", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        Customer updatedCustomer = customerService.updateCustomer(id, customer);
+        return ResponseEntity.ok(updatedCustomer);
     }
 }
