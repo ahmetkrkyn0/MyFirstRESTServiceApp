6e074918f7242a64350a3c76a7ac035785d9ab25
ahmetkrkyn0 <ahmet.karakoyun06@gmail.com>
2025-07-08 10:19:45 +0300

    added service and exception classes

--- springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/exception/CustomerNotFoundException.java
+++ springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/exception/CustomerNotFoundException.java
@@ -1,4 +1,13 @@
 package net.javaguides.springboot_first_app.exception;
 
-public class CustomerNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
 }
