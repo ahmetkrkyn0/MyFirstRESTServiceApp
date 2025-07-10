4bf7eff35bc4b66f3dc69a7dc9e198051de3b740
ahmetkrkyn0 <ahmet.karakoyun06@gmail.com>
2025-07-10 15:09:18 +0300

    Update SecurityConfig.java

--- springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/config/SecurityConfig.java
+++ springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package net.javaguides.springboot_first_app.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer; 
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -24,14 +25,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails admin = User.withUsername("admin")
-                                .password(passwordEncoder.encode("12345"))
-                                .roles("ADMIN").build();
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
 
         UserDetails user = User.withUsername("user")
-                               .password(passwordEncoder.encode("123"))
-                               .roles("USER").build();
+                .password(passwordEncoder.encode("user123"))
+                .roles("USER")
+                .build();
 
         return new InMemoryUserDetailsManager(admin, user);
     }
@@ -39,22 +42,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // REST API'ler için genellikle devre dışı bırakılır
-                .cors(cors -> {
-                }) // CORS'u etkinleştirir
-
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/api/students").hasRole("ADMIN")
-
                         .requestMatchers("/api/students/**").hasAnyRole("ADMIN", "USER")
-
                         .requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "USER")
-
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> {
-                }); 
-
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
