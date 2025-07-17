38d3dc7f721ec20edd8620edf6a1bfa3a7dd83c1
ahmetkrkyn0 <ahmet.karakoyun06@gmail.com>
2025-07-17 08:33:42 +0300

    updates

--- REST API/springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/controller/LoginController.java
+++ REST API/springboot-first-app 2/src/main/java/net/javaguides/springboot_first_app/controller/LoginController.java
@@ -34,7 +34,7 @@ public class LoginController {
         UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
         String jwt = jwtUtil.generateToken(userDetails.getUsername());
 
-        // Token'ı response olarak dön
+        // Token'ı response olarak döndürme 
         return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
